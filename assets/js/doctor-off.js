3194208c226ed77709e6864096890e942a6d3f0c
I-Ta Tsai@MBPR <ittsai@gmail.com>
2016-09-21 23:39:11 +0800

    color event by doctor's level.

--- assets/js/doctor-off.js
+++ assets/js/doctor-off.js
@@ -25,6 +25,15 @@ $(document).ready(function() {
             $("#calEventDialog").dialog("option", "title", calEvent.title);
             $('#calEventDialog').dialog('open');
             return false;   // do not follow event.url
+        },
+        eventRender: function(event, element) {
+            if (event.title.search(/主任|潘慧本|梁慧隆|吳銘庭|賴炳宏|黃哲勳|楊宗龍|周春平/) != -1) {
+                element.css('background-color', '#F44336');
+                element.css('border-color', '#F44336');
+            } else if (event.title.search(/林益輝|黃逸倫|傅瑞勛|蔡孟原|王柏欽|王俊傑|吳輔榮|江佳陵|許碩修|王威登|李明峰|何品萱/) != -1) {
+                element.css('background-color', '#009688');
+                element.css('border-color', '#009688');
+            }
         }
     });
 
