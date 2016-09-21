85ae819a19d6ec8aa587b4c7f50c647b8e724e6c
I-Ta Tsai@MBPR <ittsai@gmail.com>
2016-09-21 23:41:20 +0800

    change to a more confortable red.

--- assets/js/doctor-off.js
+++ assets/js/doctor-off.js
@@ -28,8 +28,8 @@ $(document).ready(function() {
         },
         eventRender: function(event, element) {
             if (event.title.search(/主任|潘慧本|梁慧隆|吳銘庭|賴炳宏|黃哲勳|楊宗龍|周春平/) != -1) {
-                element.css('background-color', '#F44336');
-                element.css('border-color', '#F44336');
+                element.css('background-color', '#EF5350');
+                element.css('border-color', '#EF5350');
             } else if (event.title.search(/林益輝|黃逸倫|傅瑞勛|蔡孟原|王柏欽|王俊傑|吳輔榮|江佳陵|許碩修|王威登|李明峰|何品萱/) != -1) {
                 element.css('background-color', '#009688');
                 element.css('border-color', '#009688');
